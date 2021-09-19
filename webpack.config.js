7145efb4bb716129399d44cb42d3c56238b36636
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-19 13:50:51 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/scripts/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [['@babel/preset-env', { targets: 'defaults' }]],
+        },
+      },
+    },
+  ],
+};
