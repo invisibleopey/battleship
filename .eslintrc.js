1167a174acae10ff7e61889e4a74862a646977d8
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-21 00:44:56 +0100

    Fixed esLint flagging jest test functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,13 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  rules: {
+  rules: {},
+  plugins: {
+    jest,
   },
 };
