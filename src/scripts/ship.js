b09de54452cc14e51c15b70d8571f332280b6fb7
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-23 15:59:31 +0100

    Added and tested isSunk method

--- src/scripts/ship.js
+++ src/scripts/ship.js
@@ -1,6 +1,10 @@
 const ship = function ship(name, length) {
+  const emptyCell = { name: null, hit: false };
+  let cells = new Array(length).fill(Object.create(emptyCell));
   return {
+    name,
     length,
+    isSunk: () => cells.every((cell) => cell.hit),
   };
 };
 
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,6 +1,16 @@
 import { ship } from '../scripts/ship';
 
-test('return ship object', () => {
+test('return ship object length', () => {
   let carrier = ship('carrier', 5);
   expect(carrier.length).toBe(5);
+});
+
+test('return ship object name', () => {
+  let carrier = ship('carrier', 5);
+  expect(carrier.name).toBe('carrier');
+});
+
+test('return ship object isSunk function', () => {
+  let carrier = ship('carrier', 5);
+  expect(carrier.isSunk()).toBe(false);
 });
