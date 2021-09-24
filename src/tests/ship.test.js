667dc06972b0e27b4040892af6dcec2e1a564695
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-24 17:31:09 +0100

    Grouped tests with describe and set up beforeEach function

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,39 +1,41 @@
 import { ship } from '../scripts/ship';
 
-test('tests the returned get name method', () => {
-  let carrier = ship('carrier', 4);
-  expect(carrier.name).toBe('carrier');
-});
+describe('Test ship factory', () => {
+  let carrier;
 
-test('tests the returned get length method', () => {
-  let carrier = ship('carrier', 5);
-  expect(carrier.length).toBe(5);
-});
+  beforeEach(() => {
+    carrier = ship('carrier', 5);
+  });
 
-test('tests the returned get hits method', () => {
-  let carrier = ship('carrier', 5);
-  expect(carrier.hits).toStrictEqual(['', '', '', '', '']);
-});
+  test('tests the returned get name method', () => {
+    expect(carrier.name).toBe('carrier');
+  });
 
-test('tests the returned isSunk method with false', () => {
-  let carrier = ship('carrier', 5);
-  expect(carrier.isSunk()).toBe(false);
-});
+  test('tests the returned get length method', () => {
+    expect(carrier.length).toBe(5);
+  });
 
-test('tests the returned isSunk method with true', () => {
-  let carrier = ship('carrier', 5);
-  carrier.hit(0);
-  carrier.hit(1);
-  carrier.hit(2);
-  carrier.hit(3);
-  carrier.hit(4);
-  expect(carrier.isSunk()).toBe(true);
-});
+  test('tests the returned get hits method', () => {
+    expect(carrier.hits).toStrictEqual(['', '', '', '', '']);
+  });
 
-test('tests the returned hit method', () => {
-  let carrier = ship('carrier', 5);
-  carrier.hit(2);
-  expect(carrier.hits[2]).toBe('X');
-});
+  test('tests the returned isSunk method with false', () => {
+    expect(carrier.isSunk()).toBe(false);
+  });
 
-test.todo('test the returned object of ship factory function');
+  test('tests the returned isSunk method with true', () => {
+    carrier.hit(0);
+    carrier.hit(1);
+    carrier.hit(2);
+    carrier.hit(3);
+    carrier.hit(4);
+    expect(carrier.isSunk()).toBe(true);
+  });
+
+  test('tests the returned hit method', () => {
+    carrier.hit(2);
+    expect(carrier.hits[2]).toBe('X');
+  });
+
+  test.todo('test the returned object of ship factory function');
+});
