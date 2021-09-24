39444df86c9d978da3a5be452e8e261a03bb275a
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-24 04:14:02 +0100

    Initialized the 5 ships in gameboard

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -1,7 +1,20 @@
+import { ship } from '../scripts/ship';
+
 const Gameboard = () => {
+  // Create a 10 x 10 board
   const width = 10;
   const height = 10;
   let board = new Array(width).fill(null).map((x) => new Array(height).fill(null));
+  // Initialize the 5 ships that will be required
+  const ships = [
+    ship('Carrier', 5),
+    ship('Battleship', 4),
+    ship('Destroyer', 3),
+    ship('Submarine', 3),
+    ship('Patrol Boat', 2),
+  ];
+  // Place ships
+
   return {
     board,
   };
