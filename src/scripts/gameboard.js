c3b7eb7659dab5c7d8734b6cb692a61b5dd32160
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-24 03:27:38 +0100

    Added tests for creation of the 10 X 10 board

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -1,3 +1,10 @@
-class Gameboard {}
+const Gameboard = () => {
+  const width = 10;
+  const height = 10;
+  let board = new Array(width).fill(null).map((x) => new Array(height).fill(null));
+  return {
+    board,
+  };
+};
 
 export { Gameboard };
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,3 +1,11 @@
 import { Gameboard } from '../scripts/gameboard';
 
-test.todo('');
+test('test the width of the board', () => {
+  const gameboard = Gameboard();
+  expect(gameboard.board.length).toBe(10);
+});
+
+test('test the height of the board', () => {
+  const gameboard = Gameboard();
+  expect(gameboard.board[0].length).toBe(10);
+});
