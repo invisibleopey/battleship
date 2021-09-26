b79421d6a7a1b5909e0cb9879b71ad2752bb03c7
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-26 23:23:31 +0100

    Added test for the player name returned

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,3 +1,13 @@
 import { player } from '../scripts/player';
 
-test.todo('player factory');
+describe('player factory', () => {
+  let player1;
+
+  beforeEach(() => {
+    player1 = player('opey');
+  });
+
+  test('test the player name returned', () => {
+    expect(player1.name).toBe('opey');
+  });
+});
