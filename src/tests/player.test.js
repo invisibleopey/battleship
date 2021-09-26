2f6b8ac8e79f106b4305b90fdd8b2ed5d9b59688
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-27 00:04:49 +0100

    Added test to launch random attacks

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -11,7 +11,7 @@ describe('player factory', () => {
     carrier = ship('carrier', 5);
   });
 
-  test('test the player name returned', () => {
+  test('the player name returned', () => {
     expect(player1.name).toBe('opey');
   });
 
@@ -19,13 +19,21 @@ describe('player factory', () => {
     expect(player1.positionsPrevHit).toEqual([]);
   });
 
-  test('test player attack on ship', () => {
+  test('player attack on ship', () => {
     gameboard.placeShip(carrier, 1, 1, true);
     player1.attack(1, 1, gameboard);
     player1.attack(2, 1, gameboard);
     player1.attack(3, 1, gameboard);
     player1.attack(4, 1, gameboard);
     player1.attack(5, 1, gameboard);
+    expect(gameboard.isGameOver()).toBe(true);
+  });
+
+  test('launch random attacks', () => {
+    gameboard.placeShip(carrier, 1, 1, true);
+    for (let i = 0; i < 100; i++) {
+      player1.randomAttack(gameboard);
+    }
     expect(gameboard.isGameOver()).toBe(true);
   });
 });
