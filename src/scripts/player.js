a2c198dc8b96a9c8dada90244cc194d107c26432
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-27 00:05:06 +0100

    Added a function that launches random attacks

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -7,6 +7,22 @@ const player = function player(name) {
     gameboard.receiveAttack(row, column);
   };
 
+  const randomAttack = function randomAttack(gameboard) {
+    if (positionsPrevHit.length === 100) return;
+
+    let row = Math.floor(Math.random() * 10);
+    let column = Math.floor(Math.random() * 10);
+
+    // If position has been hit, do the randomization again
+    while (hasPositionBeenPrevHit(row, column)) {
+      row = Math.floor(Math.random() * 10);
+      column = Math.floor(Math.random() * 10);
+    }
+
+    positionsPrevHit.push([row, column]);
+    gameboard.receiveAttack(row, column);
+  };
+
   function hasPositionBeenPrevHit(row, column) {
     for (let i = 0; i < positionsPrevHit.length; i++) {
       if (positionsPrevHit[i][0] === row && positionsPrevHit[i][1] === column) {
@@ -20,6 +36,7 @@ const player = function player(name) {
     name,
     positionsPrevHit,
     attack,
+    randomAttack,
   };
 };
 
