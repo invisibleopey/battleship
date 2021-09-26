6c5331ea52a4880dfe90434f160fec725ff5c34a
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-26 23:52:55 +0100

    Added attack function and a means to track previously hit positions

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,8 +1,25 @@
 const player = function player(name) {
-  const coordsPrevHit = [];
+  const positionsPrevHit = [];
+
+  const attack = function attack(row, column, gameboard) {
+    if (hasPositionBeenPrevHit(row, column)) return;
+    positionsPrevHit.push([row, column]);
+    gameboard.receiveAttack(row, column);
+  };
+
+  function hasPositionBeenPrevHit(row, column) {
+    for (let i = 0; i < positionsPrevHit.length; i++) {
+      if (positionsPrevHit[i][0] === row && positionsPrevHit[i][1] === column) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   return {
     name,
-    coordsPrevHit,
+    positionsPrevHit,
+    attack,
   };
 };
 
