b9f1fb52f3dac1a6073812cec687eacbabbfedec
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-26 01:05:37 +0100

    Added a mock to check the initial state of returned board

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -4,10 +4,23 @@ import { ship } from '../scripts/ship';
 describe('Gameboard tests', () => {
   let gameboard;
   let carrier;
+  let fakeBoard;
 
   beforeEach(() => {
     gameboard = Gameboard();
     carrier = ship('carrier', 5);
+    fakeBoard = [
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ];
   });
 
   test('test the width of the board', () => {
@@ -16,5 +29,8 @@ describe('Gameboard tests', () => {
 
   test('test the height of the board', () => {
     expect(gameboard.board[0].length).toBe(10);
+  });
+  test('the board initialization', () => {
+    expect(gameboard.board).toEqual(fakeBoard);
   });
 });
