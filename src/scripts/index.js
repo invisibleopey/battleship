74cd7cab8571204161916c3a52d96c211f4b939f
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-27 21:41:02 +0100

    Added conditionals that check if game is over at the begining of each round

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,30 +15,34 @@ renderGameboard(computerGameboard, 'computer');
 let compAttackIndex = 0;
 function gameLoop(e) {
   if (e.target.className !== 'cells') return;
-  let row = e.path[0].dataset.xIndex;
-  let column = e.path[0].dataset.yIndex;
-  player1.attack(row, column, computerGameboard);
-  if (computerGameboard.missedShots[row][column]) {
-    e.target.classList.add('miss');
-  } else {
-    e.target.classList.add('hit');
+  if (!(player1Gameboard.isGameOver() || computerGameboard.isGameOver())) {
+    let row = e.path[0].dataset.xIndex;
+    let column = e.path[0].dataset.yIndex;
+    player1.attack(row, column, computerGameboard);
+    if (computerGameboard.missedShots[row][column]) {
+      e.target.classList.add('miss');
+    } else {
+      e.target.classList.add('hit');
+    }
+    computerAttack();
   }
-  computerAttack();
 }
 
 function computerAttack() {
-  computer.randomAttack(player1Gameboard);
-  let row = computer.positionsPrevHit[compAttackIndex][0];
-  let column = computer.positionsPrevHit[compAttackIndex][1];
-  let targetCell = document.querySelector(
-    ".user-board div[data-x-index='" + row + "']" + "[data-y-index='" + column + "']",
-  );
-  if (player1Gameboard.missedShots[row][column]) {
-    targetCell.classList.add('miss');
-  } else {
-    targetCell.classList.add('hit');
+  if (!(player1Gameboard.isGameOver() || computerGameboard.isGameOver())) {
+    computer.randomAttack(player1Gameboard);
+    let row = computer.positionsPrevHit[compAttackIndex][0];
+    let column = computer.positionsPrevHit[compAttackIndex][1];
+    let targetCell = document.querySelector(
+      ".user-board div[data-x-index='" + row + "']" + "[data-y-index='" + column + "']",
+    );
+    if (player1Gameboard.missedShots[row][column]) {
+      targetCell.classList.add('miss');
+    } else {
+      targetCell.classList.add('hit');
+    }
+    compAttackIndex++;
   }
-  compAttackIndex++;
 }
 
 export { gameLoop };
