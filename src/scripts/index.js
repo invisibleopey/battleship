0f9dba729c5b5e1a705bd370511c35b91adbbd8c
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-27 15:55:58 +0100

    Initial new game implimentation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,13 @@
-function foo(x) {
-  return x * x;
-}
+import { ship } from './ship';
+import { Gameboard } from './gameboard';
+import { player } from './player';
+import { renderGameboard } from './dom-interaction';
 
-export default foo;
+const player1 = player('User');
+const computer = player('Computer');
+const player1Gameboard = Gameboard();
+const computerGameboard = Gameboard();
+player1Gameboard.placeShipsRandomly();
+computerGameboard.placeShipsRandomly();
+renderGameboard(player1Gameboard, 'player1');
+renderGameboard(computerGameboard, 'computer');
