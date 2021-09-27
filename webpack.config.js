19af5f9ae005a4dd04f471978a3d111ca3a64ceb
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-27 15:15:27 +0100

    Fixed bug of webpack not running

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,16 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  rules: [
-    {
-      test: /\.m?js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [['@babel/preset-env', { targets: 'defaults' }]],
-        },
-      },
-    },
-  ],
+  // rules: [
+  //   {
+  //     test: /\.m?js$/,
+  //     exclude: /node_modules/,
+  //     use: {
+  //       loader: 'babel-loader',
+  //       options: {
+  //         presets: [['@babel/preset-env', { targets: 'defaults' }]],
+  //       },
+  //     },
+  //   },
+  // ],
 };
