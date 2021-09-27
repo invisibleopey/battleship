e17fb28ee92f02e409cce17f2c719c509dd69f84
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-27 21:20:18 +0100

    Added logic for computerAttack and rendering the result

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,7 +12,8 @@ computerGameboard.placeShipsRandomly();
 renderGameboard(player1Gameboard, 'player1');
 renderGameboard(computerGameboard, 'computer');
 
-function attackCell(e) {
+let compAttackIndex = 0;
+function gameLoop(e) {
   if (e.target.className !== 'cells') return;
   let row = e.path[0].dataset.xIndex;
   let column = e.path[0].dataset.yIndex;
@@ -22,6 +23,22 @@ function attackCell(e) {
   } else {
     e.target.classList.add('hit');
   }
+  computerAttack();
 }
 
-export { attackCell };
+function computerAttack() {
+  computer.randomAttack(player1Gameboard);
+  let row = computer.positionsPrevHit[compAttackIndex][0];
+  let column = computer.positionsPrevHit[compAttackIndex][1];
+  let targetCell = document.querySelector(
+    ".user-board div[data-x-index='" + row + "']" + "[data-y-index='" + column + "']",
+  );
+  if (player1Gameboard.missedShots[row][column]) {
+    targetCell.classList.add('miss');
+  } else {
+    targetCell.classList.add('hit');
+  }
+  compAttackIndex++;
+}
+
+export { gameLoop };
