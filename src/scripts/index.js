7ae98761f0a3e3b224b97cd8b41d5ec9dc54d373
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-28 16:10:01 +0100

    Added event handler that captures coords entered and place ships with them

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,6 +9,7 @@ const player1Gameboard = Gameboard();
 const computerGameboard = Gameboard();
 let compAttackIndex = 0;
 
+// Random placement of ships
 const startGame = function startGame() {
   player1Gameboard.placeShipsRandomly();
   computerGameboard.placeShipsRandomly();
@@ -67,4 +68,85 @@ randomShipsBtn.addEventListener('click', () => {
   shipPlacementDiv.style.display = 'none';
 });
 
+// Placement of ships after Capturing of Coords
+const manualShipsBtn = document.querySelector('#place-ships');
+manualShipsBtn.addEventListener('click', () => {
+  let carrierX = Number(document.querySelector('#carrier-x').value);
+  let carrierY = Number(document.querySelector('#carrier-y').value);
+  let isCarrierVertical;
+  if (document.querySelector('#carrier-vertical').checked) {
+    isCarrierVertical = true;
+  } else {
+    isCarrierVertical = false;
+  }
+  player1Gameboard.placeShip(player1Gameboard.ships[0], carrierX, carrierY, isCarrierVertical);
+
+  let battleshipX = Number(document.querySelector('#battleship-x').value);
+  let battleshipY = Number(document.querySelector('#battleship-y').value);
+  let isBattleshipVertical;
+  if (document.querySelector('#battleship-vertical').checked) {
+    isBattleshipVertical = true;
+  } else {
+    isBattleshipVertical = false;
+  }
+  player1Gameboard.placeShip(
+    player1Gameboard.ships[1],
+    battleshipX,
+    battleshipY,
+    isBattleshipVertical,
+  );
+
+  let destroyerX = Number(document.querySelector('#destroyer-x').value);
+  let destroyerY = Number(document.querySelector('#destroyer-y').value);
+  let isDestroyerVertical;
+  if (document.querySelector('#destroyer-vertical').checked) {
+    isDestroyerVertical = true;
+  } else {
+    isDestroyerVertical = false;
+  }
+  player1Gameboard.placeShip(
+    player1Gameboard.ships[2],
+    destroyerX,
+    destroyerY,
+    isDestroyerVertical,
+  );
+
+  let subMarineX = Number(document.querySelector('#sub-marine-x').value);
+  let subMarineY = Number(document.querySelector('#sub-marine-y').value);
+  let isSubMarineVertical;
+  if (document.querySelector('#sub-marine-vertical').checked) {
+    isSubMarineVertical = true;
+  } else {
+    isSubMarineVertical = false;
+  }
+  player1Gameboard.placeShip(
+    player1Gameboard.ships[3],
+    subMarineX,
+    subMarineY,
+    isSubMarineVertical,
+  );
+
+  let patrolBoatX = Number(document.querySelector('#patrol-boat-x').value);
+  let patrolBoatY = Number(document.querySelector('#patrol-boat-y').value);
+  let isPatrolBoatVertical;
+  if (document.querySelector('#patrol-boat-vertical').checked) {
+    isPatrolBoatVertical = true;
+  } else {
+    isPatrolBoatVertical = false;
+  }
+  player1Gameboard.placeShip(
+    player1Gameboard.ships[4],
+    patrolBoatX,
+    patrolBoatY,
+    isPatrolBoatVertical,
+  );
+  computerGameboard.placeShipsRandomly();
+  renderGameboard(player1Gameboard, 'player1');
+  renderGameboard(computerGameboard, 'computer');
+  renderUserShips(player1Gameboard);
+
+  document.querySelector('#ships-coords-form').reset();
+  shipPlacementDiv.style.display = 'none';
+  document.querySelector('.bg-modal').style.display = 'none';
+});
 export { gameLoop };
